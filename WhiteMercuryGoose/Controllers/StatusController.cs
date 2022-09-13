25958742197b3331af8e28a1b8f61138757bcbe3
Jason Shih <jasonshih.programming@gmail.com>
2022-09-12 23:51:14 -0400

    Created Status Controller

---
+++ WhiteMercuryGoose/Controllers/StatusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WhiteMercuryGoose.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StatusController : ControllerBase
+	{
+		// GET api/<StatusController>/5
+		[HttpGet("{id}")]
+		public ActionResult Get(int id)
+		{
+			return Ok();
+		}
+	}
+}
