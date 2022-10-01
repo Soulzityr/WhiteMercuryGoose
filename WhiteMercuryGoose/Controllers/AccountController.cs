de8fbb2ec1fa6fb0e0f4d89047d60208eeb3c794
Jason Shih <jasonshih.programming@gmail.com>
2022-09-30 22:19:06 -0400

    Added Jwt Authentication Package
    .csproj included
    
    
    Fixed .json syntax error

---
+++ WhiteMercuryGoose/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using WhiteMercuryGoose.Models.Account;
+
+namespace WhiteMercuryGoose.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[AllowAnonymous]
+	public class AccountController : ControllerBase
+	{
+		// GET: api/<AccountController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<AccountController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<AccountController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<AccountController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<AccountController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+
+		[HttpPost("createToken")]
+		public void CreateToken(User user)
+		{
+
+		}
+	}
+}
--- WhiteMercuryGoose/Program.cs
+++ WhiteMercuryGoose/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace WhiteMercuryGoose
 {
 	public class Program
@@ -11,6 +15,27 @@ namespace WhiteMercuryGoose
 			builder.Logging.AddConsole();
 
 			// Add services to the container.
+
+			// Authentication
+			builder.Services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(o =>
+			{
+				o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+				{
+					ValidIssuer = builder.Configuration["Jwt:Issuer"],
+					ValidAudience = builder.Configuration["Jwt:Audience"],
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"])),
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidateLifetime = false,
+					ValidateIssuerSigningKey = true
+				};
+			});
+			builder.Services.AddAuthorization();
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,9 +52,8 @@ namespace WhiteMercuryGoose
 			}
 
 			app.UseHttpsRedirection();
-
+			app.UseAuthentication();
 			app.UseAuthorization();
-
 
 			app.MapControllers();
 
